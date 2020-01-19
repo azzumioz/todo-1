d2b98283ac0bfcf2d8285ad6b95015611ae73e19
azzumioz <my@localhost>
2020-01-19 23:02:58 +0700

    implement change style for task status

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -17,4 +17,8 @@ export class TasksComponent implements OnInit {
     this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
   }
 
+    toggleTaskCompleted (task: Task) {
+      task.completed = !task.completed;
+  }
+
 }
