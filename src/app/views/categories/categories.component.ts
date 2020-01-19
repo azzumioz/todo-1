0c67ea4937d8c12c9df96217a7aadd72cf777024
azzumioz <my@localhost>
2020-01-19 22:43:36 +0700

    *View - add to task view fields priority, date, status - add style for active cursor - use attribute directive for style categories, task completed

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -9,6 +9,7 @@ import {Category} from "../../model/Category";
 })
 export class CategoriesComponent implements OnInit {
     categories: Category[];
+    selectedCategory: Category;
 
     constructor(private dataHandler: DataHandlerService) {
     }
@@ -18,6 +19,7 @@ export class CategoriesComponent implements OnInit {
     }
 
     showTasksByCategory(category: Category) {
+        this.selectedCategory = category;
         this.dataHandler.fillTasksByCategory(category);
     }
 }
