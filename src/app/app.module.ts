377e83d205cbdf0e2d5f380f5185d15ae7859c97
azzumioz <my@localhost>
2020-01-20 22:21:28 +0700

    *Tasks - use mat-table for view table - use style for width column - change color for complete tasks

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { CategoriesComponent } from './views/categories/categories.component';
-import { TasksComponent } from './views/tasks/tasks.component';
+import {AppComponent} from './app.component';
+import {CategoriesComponent} from './views/categories/categories.component';
+import {TasksComponent} from './views/tasks/tasks.component';
+import {MatPaginatorModule, MatSortModule, MatTableModule} from "@angular/material";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    CategoriesComponent,
-    TasksComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        CategoriesComponent,
+        TasksComponent
+    ],
+    imports: [
+        BrowserModule,
+        MatTableModule,
+        MatSortModule,
+        MatPaginatorModule,
+        BrowserAnimationsModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,24 +1,51 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DataHandlerService} from "../../service/data-handler.service";
 import {Task} from "../../model/Task";
+import {MatTableDataSource} from "@angular/material";
 
 @Component({
-  selector: 'app-tasks',
-  templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+    selector: 'app-tasks',
+    templateUrl: './tasks.component.html',
+    styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
 
-  tasks: Task[];
+    // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
+    private displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
+    private dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
 
-  constructor(private dataHandler: DataHandlerService) { }
+    tasks: Task[];
 
-  ngOnInit() {
-    this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
-  }
+    constructor(private dataHandler: DataHandlerService) {
+    }
 
-    toggleTaskCompleted (task: Task) {
-      task.completed = !task.completed;
-  }
+    ngOnInit() {
+        this.dataHandler.taskSubject.subscribe(tasks => this.tasks = tasks);
+        // датасорс обязательно нужно создавать для таблицы, в него присваивается любой источник (БД, массивы, JSON и пр.)
+        this.dataSource = new MatTableDataSource();
+        this.refreshTable();
+    }
+
+    toggleTaskCompleted(task: Task) {
+        task.completed = !task.completed;
+    }
+
+    // в зависимости от статуса задачи - вернуть цвет названия
+    private getPriorityColor(task: Task) {
+        if (task.completed) {
+            return '#F8F9FA'; // TODO вынести цвета в константы (magic strings, magic numbers)
+        }
+
+        if (task.priority && task.priority.color) {
+            return task.priority.color;
+        }
+        return '#fff';
+
+    }
+
+    // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
+    private refreshTable() {
+        this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
+    }
 
 }
