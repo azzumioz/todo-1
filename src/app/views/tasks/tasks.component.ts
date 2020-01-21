f57c155831854f5de513fc0659560c548d6ac1a8
azzumioz <my@localhost>
2020-01-21 22:27:04 +0700

    *Tasks - add sort and paginator for table
    *CSS
    - add to style.css import deeppurple-amber.css

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,18 +1,22 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {DataHandlerService} from "../../service/data-handler.service";
 import {Task} from "../../model/Task";
-import {MatTableDataSource} from "@angular/material";
+import {MatPaginator, MatSort, MatTableDataSource} from "@angular/material";
 
 @Component({
     selector: 'app-tasks',
     templateUrl: './tasks.component.html',
     styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, AfterViewInit {
 
     // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
     private displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
     private dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
+
+    // ссылки на компоненты таблицы
+    @ViewChild(MatPaginator, {static: false}) private paginator: MatPaginator;
+    @ViewChild(MatSort, {static: false}) private sort: MatSort;
 
     tasks: Task[];
 
@@ -24,6 +28,11 @@ export class TasksComponent implements OnInit {
         // датасорс обязательно нужно создавать для таблицы, в него присваивается любой источник (БД, массивы, JSON и пр.)
         this.dataSource = new MatTableDataSource();
         this.refreshTable();
+    }
+
+    // в этом методе уже все проинциализировано, поэтому можно присваивать объекты (иначе может быть ошибка undefined)
+    ngAfterViewInit(): void {
+        this.addTableObjects();
     }
 
     toggleTaskCompleted(task: Task) {
@@ -46,6 +55,35 @@ export class TasksComponent implements OnInit {
     // показывает задачи с применением всех текущий условий (категория, поиск, фильтры и пр.)
     private refreshTable() {
         this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
+        this.addTableObjects();
+
+        // когда получаем новые данные..
+        // чтобы можно было сортировать по столбцам "категория" и "приоритет", т.к. там не примитивные типы, а объекты
+        // @ts-ignore - показывает ошибку для типа даты, но так работает, т.к. можно возвращать любой тип
+        this.dataSource.sortingDataAccessor = (task, colName) => {
+
+            // по каким полям выполнять сортировку для каждого столбца
+            switch (colName) {
+                case 'priority': {
+                    return task.priority ? task.priority.id : null;
+                }
+                case 'category': {
+                    return task.category ? task.category.title : null;
+                }
+                case 'date': {
+                    return task.date ? task.date : null;
+                }
+
+                case 'title': {
+                    return task.title;
+                }
+            }
+        };
+    }
+
+    private addTableObjects() {
+        this.dataSource.sort = this.sort; // компонент для сортировки данных (если необходимо)
+        this.dataSource.paginator = this.paginator; // обновить компонент постраничности (кол-во записей, страниц)
     }
 
 }
